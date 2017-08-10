7551d72f56fea3b759f1f826a95191cff8002b78
Paul Turner <programming.hero@gmail.com>
2017-08-10 20:04:37 +0100

    Completed first trivial test scenario

---
+++ test/Conker.Spec/CommandParsing.feature
@@ -0,0 +1,6 @@
+﻿Feature: Command Parsing
+
+Scenario: Invoke a command without arguments
+	Given I have a command "test" which doesn't take arguments
+	When I invoke my application with the args "test"
+	Then the "test" command is invoked
---
+++ test/Conker.Spec/CommandParsingSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace Conker.Spec
+{
+    [Binding]
+    public class CommandParsingSteps
+    {
+        private readonly Dictionary<string, Action> _commands = new Dictionary<string, Action>();
+
+        private readonly List<string> _invokedCommands = new List<string>();
+
+        [Given(@"I have a command ""(.*)"" which doesn't take arguments")]
+        public void GivenIHaveACommandWhichDoesnTTakeArguments(string commandName)
+        {
+            _commands.Add(commandName, () => _invokedCommands.Add(commandName));
+        }
+        
+        [When(@"I invoke my application with the args ""(.*)""")]
+        public void WhenIInvokeMyApplicationWithTheArgs(string args)
+        {
+            _commands[args]();
+        }
+        
+        [Then(@"the ""(.*)"" command is invoked")]
+        public void ThenTheCommandIsInvoked(string commandName)
+        {
+            _invokedCommands.Should().BeEquivalentTo(commandName);
+        }
+    }
+}
